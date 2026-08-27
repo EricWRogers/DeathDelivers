f1dec91ff5aa1b77ed4068af405648a1d12350f2
JamesonFoster <jamesonbfoster@gmail.com>
2026-08-27 17:01:29 -0500

    Scriptable Objects Set Up
    I set up the scriptable objects.

---
+++ Assets/ScriptableObject/DogBase.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "DogBase", menuName = "Scriptable Objects/DogBase")]
+public class DogBase : ScriptableObject
+{
+    public float speed = 0.5f;
+    public float turnSpeed = 0.5f; 
+    public float soulConsump = 0.5f;
+    public float scytheTurnSpeed = 0.5f;
+}
