032c64bf91d8d733fdf2ce0dfa0949f2464fb981
JamesonFoster <jamesonbfoster@gmail.com>
2026-09-03 17:02:50 -0500

    Trying To add Person Now

--- Assets/Scripts/PersonBehavior.cs
+++ Assets/Scripts/PersonBehavior.cs
@@ -2,20 +2,49 @@ using UnityEngine;
 
 public class PersonBehavior : MonoBehaviour
 {
-    public Camera camera; // optional, defaults to Camera.main
+    public Camera camera;
+    public float speed = 1.0f;
+    public Transform body;
+    public float joyAndWhimsy = 0.07f;
+    private float jaWtim = 0f;
+    private bool isUp = true;
 
     void Start()
     {
         if (camera == null)
             camera = Camera.main;
     }
+    void Update()
+    {
+        body.Translate(Vector3.forward * speed * Time.deltaTime);
+        jaWtim += Time.deltaTime;
+        if (joyAndWhimsy <= jaWtim)
+        {
+            if (isUp)
+                isUp = false;
+            else
+                isUp = true;
+        }
+        else
+        {
+            if (isUp)
+                transform.Translate(Vector3.up * 3.0f * Time.deltaTime);
+            else
+                transform.Translate(Vector3.down * 3.0f * Time.deltaTime);
+        }
+    }
 
     void LateUpdate()
     {
         // Make the object look at the camera
-        transform.LookAt(camera.transform + (transform.localRotation.x + 90));
+        transform.LookAt(camera.transform);
+        // Get current rotation in Euler angles
+        Vector3 currentRotation = transform.eulerAngles;
 
-        // Optional: lock vertical rotation to avoid flipping
-        // transform.localRotation = Quaternion.Euler(90f, transform.rotation.y, 0);
+        // Add 90 degrees to the X axis
+        currentRotation.x += 90f;
+
+        // Apply the new rotation
+        transform.eulerAngles = currentRotation;
     }
 }
